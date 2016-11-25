d63a38a011a1eea7b346a0978c4637aee9a9ec54
Nguyen, Phong Thanh <PhongThanh.Nguyen@contractor.ca.com>
2016-11-25 18:16:49 +0700

    aaaa

---
+++ src/ui/database/mgm/filter/AuthenticationLoginFilter.java
@@ -0,0 +1,52 @@
+package ui.database.mgm.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationLoginFilter implements Filter {
+
+	private ServletContext context;
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String uri = req.getRequestURI();
+		this.context.log("Requested Resource::"+uri);
+		
+		HttpSession session = req.getSession(false);
+		
+		if(session == null || (session != null && session.getAttribute("currentUser") == null)){
+			this.context.log("Unauthorized access request");
+			res.sendRedirect(req.getContextPath() + "/home");
+		}else{
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
